02b805827d201b26d4faa0afbbe3b61ea0e7b539
kamikaonashi <thorschi93@gmx.at>
2026-06-23 21:39:29 +0200

    graphene: add build flag
    clone this repo to your rom vendor and call via
    TARGET_BUILD_GRAPHENEAPPS := true
    in your device tree

--- Android.mk
+++ Android.mk
@@ -1,5 +1,8 @@
-# GrapheneOs Camera
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_BUILD_GRAPHENEAPPS), true)
 PRODUCT_PACKAGES += \
     Camera \
     AppStore \
     privapp-permissions-app.grapheneos.apps.xml
+endif
